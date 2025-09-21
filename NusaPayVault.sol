148fd73a5f171068b4dd53ef9f3d41c4b36c0559
Nusa Corporation <nusapay@myyahoo.com>
2025-09-21 13:27:02 +0200

    Create NusaPayVault.sol

---
+++ NusaPayVault.sol
@@ -0,0 +1,9 @@
+contract NusaPayVault {
+    mapping(address => uint256) public balances;
+    function deposit() external payable { balances[msg.sender] += msg.value; }
+    function withdraw(uint256 amount) external { 
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+}
